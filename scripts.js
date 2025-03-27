b5554652a07f763d5071ba7f4fc244510f3b298b
Vini7788 <vinileme2015@gmail.com>
2025-03-27 18:00:37 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Este JavaScript pode ser utilizado para futuras interações, animações ou validações, mas para este exemplo, ele é opcional
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Site carregado com sucesso!");
+});
